ba0cde19f7cca989e2e81037a8b5360c80436a32
Effectivement <richel265@tuta.io>
2026-04-02 16:25:23 -0400

    Update clear color to be transparent and add window bg color

--- src/main.cpp
+++ src/main.cpp
@@ -171,7 +171,9 @@ int main(int, char **) {
   // Our state
   bool show_demo_window = true;
   bool show_another_window = false;
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  ImVec4 clear_color = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  ImVec4 window_bg_color = ImVec4(0.00f, 0.00f, 0.00f, 0.70f);
 
   bool fahrenheit = false;
   float cpu_temp_celsius = 0;
