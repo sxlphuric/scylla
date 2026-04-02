d6b7c28521da151796acff340ee46839265e44f0
Effectivement <richel265@tuta.io>
2026-04-02 16:26:00 -0400

    Allow transparent framebuffer

--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,7 @@ int main(int, char **) {
   // Create window with graphics context
   float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(
       glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
+  glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
   GLFWwindow *window =
       glfwCreateWindow((int)(1280 * main_scale), (int)(800 * main_scale),
                        "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
